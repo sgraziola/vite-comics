d60aff0d3b9f6773180320c8e981c02c462166ba
sgraziola <simo.fdl@gmail.com>
2022-11-11 16:30:39 +0100

    Vite-comics: Add style to thumbs in comicList

--- src/components/Comic.vue
+++ src/components/Comic.vue
@@ -12,15 +12,11 @@ export default {
     <div class="col">
         <div class="comic">
             <img :src="thumb" :alt="title">
-            <h6>{{ title }}</h6>
+            <span>{{ title }}</span>
         </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
-.comic {
-    h6 {
-        text-transform: uppercase;
-    }
-}
+
 </style>
--- src/components/ComicsList.vue
+++ src/components/ComicsList.vue
@@ -16,18 +16,29 @@ export default {
 
 <template>
 
-    <div class="container">
-        <button class="main_btn shadow mb-4"><a href="#">CURRENT SERIES</a></button>
+    <div class="container m_neg">
+        <button class="main_btn btn_big shadow mb-4"><a href="#">CURRENT SERIES</a></button>
 
         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4 row-cols-lg-6 g-5">
 
             <Comic v-for="Comic in comics" :thumb="Comic.thumb" :title="Comic.series" />
 
         </div>
+        <div class="load text-center">
+            <button class="main_btn btn_reg shadow mb-4"><a href="#">LOAD MORE</a></button>
+        </div>
     </div>
 
 </template>
 
 <style lang="scss" scoped>
+.btn_big {
+    font-size: 1.7rem;
+}
 
+.btn_reg {
+    margin-top: 3rem;
+    width: 200px;
+    font-size: 14px;
+}
 </style>
